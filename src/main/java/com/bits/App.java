102df1c4a23e36275ed223156caf71bd30084577
2022wc86397 <2022wc86397@wilp.bits-pilani.ac.in>
2026-09-16 21:05:40 +0530

    Fix typo in print statement in App.java

--- src/main/java/com/bits/App.java
+++ src/main/java/com/bits/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello Jenkins CI");
         System.out.println("Build Successful Using Maven and Jenkins");
+        system.out.println("2022wc86397")
     }
 }

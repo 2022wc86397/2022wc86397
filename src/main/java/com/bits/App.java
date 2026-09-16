7d1f2a8f2addd48d7f936cce3e829a9903e358c8
2022wc86397 <2022wc86397@wilp.bits-pilani.ac.in>
2026-09-16 21:09:36 +0530

    Modify greeting and fix print statement casing
    Updated greeting message and fixed casing in print statement.

--- src/main/java/com/bits/App.java
+++ src/main/java/com/bits/App.java
@@ -2,8 +2,7 @@ package com.bits;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins CI");
+        System.out.println("Hello  CI");
         System.out.println("Build Successful Using Maven and Jenkins");
-        system.out.println("2022wc86397")
     }
 }

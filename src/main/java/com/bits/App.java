d7a1dcc1aac22ddb8cdf30b5118d2cbfee43d95f
2022wc86397 <2022wc86397@wilp.bits-pilani.ac.in>
2026-09-16 21:16:32 +0530

    Update greeting message to include 'Jenkins'

--- src/main/java/com/bits/App.java
+++ src/main/java/com/bits/App.java
@@ -2,7 +2,7 @@ package com.bits;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello  CI");
+        System.out.println("Hello Jenkins CI");
         System.out.println("Build Successful Using Maven and Jenkins");
     }
 }

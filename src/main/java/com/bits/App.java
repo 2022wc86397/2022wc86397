1545d98e101a94e7fa93e1eff2a77d8abe5eed3b
2022wc86397 <2022wc86397@wilp.bits-pilani.ac.in>
2026-09-16 20:45:44 +0530

    Add App class with main method for DevOps message

---
+++ src/main/java/com/bits/App.java
@@ -0,0 +1,8 @@
+package com.bits;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello DevOps");
+        System.out.println("Build Successful Using Maven and Jenkins");
+    }
+}

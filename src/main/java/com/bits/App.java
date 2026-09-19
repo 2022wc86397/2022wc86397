4169d6750408fe0ecfadd18627e8744c4418fe69
2022wc86397 <2022wc86397@wilp.bits-pilani.ac.in>
2026-09-19 17:04:20 +0530

    Add additional output message to App.java

--- src/main/java/com/bits/App.java
+++ src/main/java/com/bits/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello DevOps");
         System.out.println("Build Successful Using Maven and Jenkins");
+        System.out.println("2022wc86397");
     }
 }
